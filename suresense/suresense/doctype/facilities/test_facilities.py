caaf282836a7dfa2cbc97d5a537e506367c71e29
Rifaz Marikkar <rifaz.marikkar@hotmail.com>
2023-11-25 05:38:48 +0000

    first commit

---
+++ suresense/suresense/doctype/facilities/test_facilities.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Rifaz Marikkar and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestFacilities(FrappeTestCase):
+	pass
---
+++ suresense/suresense/doctype/tenants/test_tenants.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Rifaz Marikkar and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTenants(FrappeTestCase):
+	pass
